6f315b75a65113d4ad641fb717fc0916cf23f0c4
Ahmed <ghoul.ahmed5@gmail.com>
2017-10-28 18:31:03 +0100

    create main.ts + first component  + first module +.vscode settings

---
+++ app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-root',
+    template: `    <h1>Angular Project </h1>  `
+})
+export class AppComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
